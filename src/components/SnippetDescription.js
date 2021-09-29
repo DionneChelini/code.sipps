72bc752ae7332065759cddb8341d7148042cf3a2
Dionne Chelini <purplestreetsmarts@gmail.com>
2021-09-30 00:32:08 +1000

    moved e.preventDefault

--- src/components/SnippetDescription.js
+++ src/components/SnippetDescription.js
@@ -1,9 +1,9 @@
-import React, { useContext, useState, useRef } from 'react';
-import UserContext from '../context/user/userContext';
-import { faDatabase } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { v4 as uuid4 } from 'uuid';
-import AlertContext from '../context/alert/alertContext';
+import React, { useContext, useState, useRef } from "react";
+import UserContext from "../context/user/userContext";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { v4 as uuid4 } from "uuid";
+import AlertContext from "../context/alert/alertContext";
 
 const SnippetDescription = ({ value }) => {
   const userContext = useContext(UserContext);
@@ -14,10 +14,10 @@ const SnippetDescription = ({ value }) => {
 
   const [card, setCard] = useState({
     id: uuid4(),
-    title: '',
-    subtitle: '',
-    snippet: '',
-    source: '',
+    title: "",
+    subtitle: "",
+    snippet: "",
+    source: "",
   });
 
   // useEffect(() => {}, [renderEditor]);
@@ -30,26 +30,26 @@ const SnippetDescription = ({ value }) => {
     });
 
   const onSubmit = (e) => {
+    e.preventDefault();
     if (!card.title && !card.subtitle) {
-      setAlert('Please include title and subtitle', 'bg-yellow');
+      setAlert("Please include title and subtitle", "bg-yellow");
       return;
     }
     if (!card.title) {
-      setAlert('Please include title', 'bg-yellow');
+      setAlert("Please include title", "bg-yellow");
       return;
     }
     if (!card.subtitle) {
-      setAlert('Please include subtitle', 'bg-yellow');
+      setAlert("Please include subtitle", "bg-yellow");
       return;
     }
     if (!value) {
-      setAlert('Please include code snippet', 'bg-yellow');
+      setAlert("Please include code snippet", "bg-yellow");
       return;
     }
 
-    setAlert('New code snippet saved to MongoDB', 'bg-green');
+    setAlert("New code snippet saved to MongoDB", "bg-green");
     createCard(card);
-    e.preventDefault();
   };
   return (
     <div className='pl-1 text-dark f-16 titillium'>
@@ -62,7 +62,7 @@ const SnippetDescription = ({ value }) => {
             placeholder='javascript'
             type='text'
             className='dotted-bottom-border titillium f-27 text-dark m-0 p-0 ptp-1 text-center w-25 bg-light-grey'
-            style={{ height: '27px' }}
+            style={{ height: "27px" }}
           />
           <p className='text-dark mrp-4'>and I've just learned how to</p>
           <input
@@ -71,13 +71,13 @@ const SnippetDescription = ({ value }) => {
             placeholder='import a library'
             type='text'
             className='dotted-bottom-border titillium f-27 text-dark bg-light-grey m-0 p-0 ptp-1  text-center w-25'
-            style={{ height: '27px' }}
+            style={{ height: "27px" }}
           />
         </div>
         <div className='mt-1'>
           <p className='text-dark mrp-4 titilliumRegular'>
             <span className='text-black titillium'>Source URL:</span> where does
-            this information come from? (optional){' '}
+            this information come from? (optional){" "}
           </p>
           <div className='d-flex justify-content-space-between align-items-center'>
             <input
@@ -93,7 +93,7 @@ const SnippetDescription = ({ value }) => {
                 type='submit'
                 className='bg-dark btn btn-lg text-light-blue z-depth-1 B612 mt-1'
               >
-                {' '}
+                {" "}
                 <FontAwesomeIcon
                   icon={faDatabase}
                   className='text-light-blue font-weight-light prp-5'
